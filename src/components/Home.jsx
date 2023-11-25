595d35ed1f6a8d06d06cc58733407c3f447ce078
rashaabdulrazzak <rasha.abdulrazzak.com>
2023-11-25 18:17:05 +0300

    create nav ba

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>llllllllllllllllllllllllllllll llllllllllllllllll</p>
+    </div>
+  );
+};
+
+export default Home;
